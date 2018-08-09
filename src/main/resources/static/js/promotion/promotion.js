b42bab7a531121034faf410d88f01735870ffc1a
우아테크캠프_2 <woowatechcamp_2@woowatechcamp2.local>
2018-08-10 01:54:15 +0900

    배민찬, 프로모션 transition 전환효과

--- src/main/resources/static/js/promotion/promotion.js
+++ src/main/resources/static/js/promotion/promotion.js
@@ -2,35 +2,103 @@ function nextArrowBtnHandler(evt) {
     evt.preventDefault();
     let currentPromotion;
     let promotionArr = $("#promotion-img-box").querySelectorAll("li");
-    // for of는 break문이 동작, foreach는 안
+    // for of는 break문이 동작, foreach는 안멈춤
     for(let promotion of promotionArr) {
         if(promotion.classList.contains("current")) {
             promotion.classList.remove("current");
             currentPromotion = promotion.nextElementSibling || promotion.parentElement.firstElementChild;
             currentPromotion.classList.add("current");
-            return false;
+            break;
         }
     }
+    highlightNextDot();
 }
 
 function prevArrowBtnHandler(evt) {
     evt.preventDefault();
     let currentPromotion;
     let promotionArr = $("#promotion-img-box").querySelectorAll("li");
-    // for of는 break문이 동작, foreach는 안
+    // for of는 break문이 동작, foreach는 안멈춤
     for(let promotion of promotionArr) {
         if(promotion.classList.contains("current")) {
             promotion.classList.remove("current");
             currentPromotion = promotion.previousElementSibling || promotion.parentElement.lastElementChild;
             currentPromotion.classList.add("current");
-            return false;
+            break;
         }
     }
+    highlightPrevDot();
+}
+
+function highlightNextDot() {
+    let currentDot;
+    let dotArr = $("#dot-btn-box").querySelectorAll("a");
+    for(let dot of dotArr) {
+        if(dot.classList.contains("on")) {
+            dot.classList.remove("on");
+            currentDot = dot.nextElementSibling || dot.parentElement.firstElementChild;
+            currentDot.classList.add("on");
+            break;
+        }
+    }
+}
+
+function highlightPrevDot() {
+    let currentDot;
+    let dotArr = $("#dot-btn-box").querySelectorAll("a");
+    for(let dot of dotArr) {
+        if(dot.classList.contains("on")) {
+            dot.classList.remove("on");
+            currentDot = dot.previousElementSibling || dot.parentElement.lastElementChild;
+            currentDot.classList.add("on");
+            break;
+        }
+    }
+}
+
+function removeHighlightDot() {
+    let dotArr = $("#dot-btn-box").querySelectorAll("a");
+    for(let dot of dotArr) {
+        if(dot.classList.contains("on")) {
+            dot.classList.remove("on");
+            break;
+        }
+    }
+}
+
+function removeCurrentPromotion() {
+    let promotionArr = $("#promotion-img-box").querySelectorAll("li");
+    for(let promotion of promotionArr) {
+        if(promotion.classList.contains("current")) {
+            promotion.classList.remove("current");
+            break;
+        }
+    }
+}
+
+function changeClickedPromotion(targetPosition) {
+    $("#promotion-img-box").querySelectorAll("li")[targetPosition].classList.add("current");
+}
+
+function isNotDot(target) {
+    return $("#dot-btn-box").isEqualNode(target);
+}
+
+function moveClickedDotHandler(evt) {
+    evt.preventDefault();
+    if(isNotDot(evt.target) === true) {
+        return false;
+    }
+    removeHighlightDot();
+    evt.target.classList.add("on");
+    removeCurrentPromotion();
+    changeClickedPromotion(Array.from($("#dot-btn-box").querySelectorAll("a")).indexOf(evt.target));
 }
 
 function setEventListener() {
     $("#btnPrevArrow").addEventListener("click", prevArrowBtnHandler);
     $("#btnNextArrow").addEventListener("click", nextArrowBtnHandler);
+    $("#dot-btn-box").addEventListener("click", moveClickedDotHandler);
 }
 
 document.addEventListener("DOMContentLoaded", () => {
